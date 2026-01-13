aea1adea497263b79f8119d12b9535e38a9ac8f6
Kamiikrazy <bobmcbob717@gmail.com>
2026-01-13 23:14:49 +0000

    nightly build

--- src/modules/cards.py
+++ src/modules/cards.py
@@ -1,23 +1,35 @@
 class Cards:
-    def __init__(self, generic_mana, sp_mana, card_type, description):
+    def __init__(self, name, generic_mana, sp_mana, card_type, description):
+        self.name = name
         self.generic_mana = generic_mana
         self.sp_mana = sp_mana
         self.type = card_type
-        self.description 
+        self.description = description
+        self.tapped = 0     # 0 means untapped, 1 means tapped
+        self.status = ""
 
-# Subclass for creatures
-class CreatureCard(Cards):
-    def __init__(self, generic_mana, sp_mana, description, att, end):
-        super().__init__(generic_mana, sp_mana, "Creature", description)
+# Subclass for summons
+class SummonCard(Cards):
+    def __init__(self, name, generic_mana, sp_mana, description, att, end, effect, status):
+        super().__init__(name, generic_mana, sp_mana, "Creature", description)
         self.attack = att
         self.defence = end
+        self.effect = effect
+        self.status = status
 
+# Subclass for spells
 class SpellCard(Cards):
-    def __init__(self, generic_mana, sp_mana, description, effect):
-        super().__init__(generic_mana, sp_mana, "Spell", description)
+    def __init__(self, name, generic_mana, sp_mana, description, effect):
+        super().__init__(name, generic_mana, sp_mana, "Spell", description)
         self.effect = effect
-        
+
+# Subclass for enchantments        
 class EnchantmentCards(Cards):
-    def __init__(self, generic_mana, sp_mana, description, effect):
-        super().__init__(generic_mana, sp_mana, "Enchantment", description)
+    def __init__(self, name, generic_mana, sp_mana, description, effect):
+        super().__init__(name, generic_mana, sp_mana, "Enchantment", description)
         self.effect = effect
+
+# Subclass for Lands
+class LandCards(Cards):
+    def __init__(self, name, generic_mana, sp_mana, description, ):
+        super().__init__(name, generic_mana, sp_mana, "Land", description)
